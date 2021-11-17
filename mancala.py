ab6900146faf33fd988be8f0a56fd45af7926c35
Bones Malin <carlodelpizzo@gmail.com>
2021-11-17 16:23:30 -0500

    unrelated

--- mancala.py
+++ mancala.py
@@ -13,9 +13,9 @@ class Mancala:
         self.game_over = False
         self.move_history = []
 
-    def move_is_valid(self, pot_choice: int):
-        pot_choice = pot_choice % (self.max_index + 1)
-        if self.board[self.current_player][pot_choice] != 0:
+    def move_is_valid(self, hole_choice: int):
+        hole_choice = hole_choice % (self.max_index + 1)
+        if self.board[self.current_player][hole_choice] != 0:
             return True
         else:
             return False
@@ -39,15 +39,14 @@ class Mancala:
                     self.board[other_i][i] = 0
             self.game_over = True
 
-    def current_player_move(self, pot_choice: int):
-        # print('before player ' + str(self.current_player) + ' move: ', self.board)
-        pot_choice = pot_choice % (self.max_index + 1)
+    def current_player_move(self, hole_choice: int):
+        hole_choice = hole_choice % (self.max_index + 1)
         self.move_count += 1
-        hand_index = pot_choice + 1
+        hand_index = hole_choice + 1
         current_side = self.current_player
-        if self.board[self.current_player][pot_choice] != 0:
-            self.hand = self.board[self.current_player][pot_choice]
-            self.board[self.current_player][pot_choice] = 0
+        if self.board[self.current_player][hole_choice] != 0:
+            self.hand = self.board[self.current_player][hole_choice]
+            self.board[self.current_player][hole_choice] = 0
         if self.hand > 0:
             while self.hand > 0:
                 if 0 <= hand_index <= self.max_index:
@@ -75,10 +74,7 @@ class Mancala:
             self.board[other_i][(len(self.board[other_i]) - 1) - (hand_index - 1)] = 0
 
         self.ending_condition_check()
-        self.move_history.append([self.current_player, pot_choice])
-
-        # print('player ' + str(self.current_player) + ' plays pot ' + str(pot_choice) + ': ', self.board,
-        #       '\nScore: ', self.pot, '\n# of Moves: ', self.move_count, '\n----')
+        self.move_history.append([self.current_player, hole_choice])
 
         if hand_index == 0 and current_side != self.current_player:
             pass
@@ -135,27 +131,33 @@ def overwrite_history_file(lines: list):
 
 game = Mancala()
 
-write_lines_to_history_file(['*****************************NEW INSTANCE*****************************\n'])
+# write_lines_to_history_file(['*****************************NEW INSTANCE*****************************\n'])
 
 max_moves = 0
-max_depth = 1000
-counter = 0
+max_depth = 1000000
+counter = 1
 gaming = True
 while gaming:
     while not game.game_over:
         game.random_hole_strategy()
+
     if game.move_count > max_moves:
         max_moves = game.move_count
         print('current highest: ', max_moves)
-        history_lines = ['Current Highest # of Moves: ' + str(max_moves) + '\n']
-        for move in game.move_history:
-            history_lines.append(str(move[0] + 1) + ', ' + str(move[1] + 1) + '\n')
-        history_lines.append('---------\n')
-        write_lines_to_history_file(history_lines)
-        counter = 0
+
+        # history_lines = ['Current Highest # of Moves: ' + str(max_moves) + '\n']
+        # for move in game.move_history:
+        #     history_lines.append(str(move[0] + 1) + ', ' + str(move[1] + 1) + '\n')
+        # history_lines.append('---------\n')
+        # write_lines_to_history_file(history_lines)
+
     game.__init__()
-    # counter += 1
-    # if counter == max_depth:
-    #     gaming = False
+
+    if counter >= max_depth:
+        gaming = False
+    counter += 1
+
+    if counter % 1000 == 0:
+        print(counter)
 
 print('highest: ', max_moves)
